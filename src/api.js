41ae6215dcebb798f128c4449d3c7994ac1ed2e3
0SahilUK77 <sahilullah77@gmail.com>
2026-05-08 20:14:45 +0530

    Update API base URL to production environment

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/", // Update with your backend API URL
+  baseURL: "https://teamtaskmanagerbackend-production-aa48.up.railway.app/",
 });
 
 // Add a request interceptor to attach the JWT token
